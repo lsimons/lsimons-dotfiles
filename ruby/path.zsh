4bcb1759b149e8faf93616ae871bb96507ed73ef
Leo Simons <mail@leosimons.com>
2026-02-02 08:30:21 +0100

    feat(ruby): Add 4.0.0 gem bin to PATH
    Co-authored-by: lsimons-bot <bot@leosimons.com>

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,3 +1,7 @@
 if [[ -d "/opt/homebrew/opt/ruby/bin" ]]; then
   export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 fi
+
+if [[ -d "/Users/lsimons/.gem/ruby/4.0.0/bin" ]]; then
+  export PATH="/Users/lsimons/.gem/ruby/4.0.0/bin:$PATH"
+fi
